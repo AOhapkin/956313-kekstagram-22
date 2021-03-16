b2e33d50df948574d92881ade81088413d2e4d84
AOhapkin <alexeyyago@gmail.com>
2021-03-16 11:11:04 +0300

    Проверка тегов на дубли в любом регистре

--- js/hashtag-validation.js
+++ js/hashtag-validation.js
@@ -1,5 +1,6 @@
 const tagsInput = document.querySelector('.text__hashtags');
 const MAX_TAGS_NUMBER = 5;
+const MAX_TAGS_LENGTH = 20;
 const pattern = /[^a-zA-Z0-9]/;
 
 tagsInput.addEventListener('input', () => {
@@ -11,8 +12,30 @@ tagsInput.addEventListener('input', () => {
       tagsInput.setCustomValidity('Максимальное число тегов: ' + MAX_TAGS_NUMBER);
     } else if (pattern.test(tags[i].slice(1))) {
       tagsInput.setCustomValidity('Теги должны состоять только из букв и числел');
+    } else if (tags[i].length > MAX_TAGS_LENGTH) {
+      tagsInput.setCustomValidity('Слишком длинный тег. Максимум символов: ' + MAX_TAGS_LENGTH);
+    } else if (tags[i].length === 1) {
+      tagsInput.setCustomValidity('Тег не может быть пустым');
+    } else if (hasDuplicates(tags)) {
+      tagsInput.setCustomValidity('Удалите повторяющиеся теги');
     } else {
       tagsInput.setCustomValidity('');
     }
   }
-})
+});
+
+function hasDuplicates (array) {
+  let valuesSoFar = Object.create(null);
+  for (let i = 0; i < array.length; ++i) {
+    let value = array[i].toLowerCase();
+    if (value in valuesSoFar) {
+      return true;
+    }
+    valuesSoFar[value] = true;
+  }
+  return false;
+}
+
+
+
+
