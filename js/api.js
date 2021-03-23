2dcfdab6a22f5febc9e7edbef481c77c6c5489ba
AOhapkin <alexeyyago@gmail.com>
2021-03-23 17:51:54 +0300

    Переписал showError

--- js/api.js
+++ js/api.js
@@ -3,14 +3,13 @@ import {isEscEvent} from './utils.js';
 const errorTemplate = document.querySelector('#error').content;
 const errorMessage = errorTemplate.querySelector('.error').cloneNode(true);
 const errorCloseButton = errorMessage.querySelector('.error__button');
-const errorText = errorMessage.querySelector('.error__title');
+// const errorText = errorMessage.querySelector('.error__title');
 const MESSAGE_TIME = 4000;
 
 function getData () {
   return fetch('https://22.javascript.pages.academy/kekstagram/data')
     .then(checkStatus)
-    .then((response) => response.json())
-    .catch(showError);
+    .then((response) => response.json());
 }
 
 function checkStatus(response) {
@@ -18,20 +17,27 @@ function checkStatus(response) {
     return response;
   }
 
-  const {statusText, status} = response;
-  showError(statusText, status);
+  showError('Не удалось получить данные. Попробуйте позже.');
 }
 
-function showError (statusText, status) {
-  errorMessage.style.zIndex = '100';
-  document.body.append(errorMessage);
-  errorText.textContent = status + ' ' + statusText;
-  errorCloseButton.textContent = 'Ок';
-  errorCloseButton.addEventListener('click', onErrorCloseButtonClick);
-  document.addEventListener('keydown', onDocumentKeydown);
+function showError (message) {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
 
   setTimeout(() => {
-    hideDownloadError();
+    alertContainer.remove();
   }, MESSAGE_TIME);
 }
 
