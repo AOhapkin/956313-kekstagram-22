d4753aa412eaa6909970e761f0ae594e69711076
AOhapkin <alexeyyago@gmail.com>
2021-03-26 16:27:14 +0300

    Добавил проверку нажатия на img-filters__button

--- js/pictures-filter.js
+++ js/pictures-filter.js
@@ -1,11 +1,17 @@
 const filter = document.querySelector('.img-filters');
 const filterButtons = filter.querySelectorAll('.img-filters__button');
+const FILTER_ACTIVE_CLASS = 'img-filters__button--active'
 
 function updateFilter (evt) {
+  if (!evt.target.classList.contains('img-filters__button')) {
+    return;
+  }
   filterButtons.forEach((button) => {
-    button.classList.remove('img-filters__button--active');
+    button.classList.remove(FILTER_ACTIVE_CLASS);
   })
-  evt.target.classList.add('img-filters__button--active');
+  evt.target.classList.add(FILTER_ACTIVE_CLASS);
 }
+
+
 
 export {updateFilter}
